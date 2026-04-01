04684f4a53885200b88a31cc2892e56e7976968f
con ca ngap ngap <giauntn7@gmail.com>
2026-04-01 23:27:27 +0700

    weather-object

---
+++ weather-object.js
@@ -0,0 +1,8 @@
+let currentWeather= {
+    temperature : 25,
+    conditions : 'cloudy',
+}
+
+console.log(currentWeather.temperature);
+console.log(currentWeather.conditions);
+console.log(`It's ${currentWeather.temperature} degrees F and ${currentWeather.conditions}.`)
