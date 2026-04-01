6f678bf9f69cff014c8cfbe05cdcda7e8f6240a1
con ca ngap ngap <giauntn7@gmail.com>
2026-04-01 23:33:17 +0700

    xoa file

--- today-weather.js
+++
@@ -1,5 +0,0 @@
-let temperature= 25;
-let conditions='cloudy';
-let sunriseTime='7:45PM';
-let sunsetTime='3:30AM';
-console.log(`It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`)
